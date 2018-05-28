76b6548b5bd40f3644318cb00f3d697eb33fe7f9
Salvador Guzman <guzmansalv@gmail.com>
2018-05-28 07:43:47 -0700

    check env var PORT

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,7 @@ using std::cerr;
 using std::endl;
 
 constexpr static const char * const address{"0.0.0.0"};
-constexpr const static uint16_t cores{8};
-constexpr const static uint16_t port{8080};
+constexpr const static unsigned short cores{8};
 
 static inline const char* resp(
     const std::string& user,
@@ -18,7 +17,30 @@ static inline const char* resp(
     return (user + repo + branch + file).c_str();
 }
 
-int main() noexcept {
+int main(int, char*[], char* env[]) noexcept {
+    unsigned short port{};
+    char port_str[5]{};
+    for (char** e{env}; *e != nullptr; ++e) {
+      char* str{*e};
+      if (str[0] == 'P' && str[1] == 'O' && str[2] == 'R' && str[3] == 'T' && str[4] == '=') {
+        for (uint8_t i{0}; str[i] != '\0'; ++i) {
+          port_str[i] = str[i];
+        }
+
+        port = static_cast<unsigned short>(atoi(port_str));
+        cout << "Found PORT env var with val " << port << endl;
+
+        break;
+      }
+    }
+
+    if (port_str[0] == '\0') {
+      port = 8080;
+      cout << "Defaulting to port " << port << endl;
+    }
+
+
+
     {
         crow::SimpleApp app{};
         CROW_ROUTE(app, "/<string>/<string>/<string>/<string>")(resp);
